6bff73224f179fc7f88d7f85cfe83501ea0cf146
Marek Hulan <mhulan@redhat.com>
2013-12-11 14:18:24 +0100

    Fixed JS loading on pages without jsToolBar

--- assets/javascripts/prer.js
+++ assets/javascripts/prer.js
@@ -1,13 +1,15 @@
 $(function () {
-  // pre with code class ruby
-  jsToolBar.prototype.elements.prer = {
-          type: 'button',
-          title: 'Ruby example',
-          fn: {
-                  wiki: function() { this.encloseLineSelection('<pre><code class="ruby">\n', '\n</code></pre>') }
-          }
-  }
+  if (typeof jsToolBar != 'undefined') {
+    // pre with code class ruby
+    jsToolBar.prototype.elements.prer = {
+            type: 'button',
+            title: 'Ruby example',
+            fn: {
+                    wiki: function() { this.encloseLineSelection('<pre><code class="ruby">\n', '\n</code></pre>') }
+            }
+    }
 
-  // redraw existing toolbar
-  wikiToolbar.draw();
+    // redraw existing toolbar
+    wikiToolbar.draw();
+  }
 });
